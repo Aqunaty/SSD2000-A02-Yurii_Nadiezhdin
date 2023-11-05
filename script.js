d404847ddc76d9fdf014e2e916c69b4823be16b7
Yurii Nadiezhdin <ynadiezhdin@my.bcit.ca>
2023-11-05 10:34:02 +0200

    some green stuff

--- script.js
+++ script.js
@@ -1,1 +1,12 @@
 'use strict';
+const game =
+{
+    title:"",
+    isRunning: false,
+    currentScreen: document.getElementsByClassName("screen active-screen")[0].id,
+
+    toggleGame: function() 
+    {
+        this.isRunning = !this.isRunning;
+    }
+}
